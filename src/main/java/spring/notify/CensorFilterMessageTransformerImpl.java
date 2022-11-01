9fb810e699adf9b52c482323b1c822bb8c33dbcf
Ekaterina Matkovskaya <frau.fedorchenko2014@yandex.ru>
2022-11-01 15:27:38 +0300

    MessageTransformer issues

--- src/main/java/spring/notify/CensorFilterMessageTransformerImpl.java
+++ src/main/java/spring/notify/CensorFilterMessageTransformerImpl.java
@@ -12,20 +12,24 @@ public class CensorFilterMessageTransformerImpl implements MessageTransformer {
         String toLowerCase = message.toLowerCase();
         String[] strings = toLowerCase.split(" ");
         StringBuilder sb = new StringBuilder();
-        Map<Integer,String> badWords = new HashMap<>();
-        badWords.put(1,"bad");
-        badWords.put(2,"baddy");
-        badWords.put(3,"badly");
-        badWords.put(4,"worse");
-        badWords.put(5,"worst");
+
         for (int i = 0; i < strings.length; i++) {
-            boolean isBadWord = badWords.containsValue(strings[i]);
-            if(isBadWord){
+            if (isBadWord(strings[i])) {
                 sb.append("****").append(" ");
             } else {
                 sb.append(strings[i]).append(" ");
             }
         }
         return sb.toString();
+    }
+
+    private boolean isBadWord(String word) {
+        Map<Integer, String> badWords = new HashMap<>();
+        badWords.put(1, "bad");
+        badWords.put(2, "baddy");
+        badWords.put(3, "badly");
+        badWords.put(4, "worse");
+        badWords.put(5, "worst");
+        return badWords.containsValue(word);
     }
 }
