b4337e837dfcddbd94526e0081cead8599dec1b9
Ekaterina Matkovskaya <frau.fedorchenko2014@yandex.ru>
2022-11-21 19:27:12 +0300

    added new Service that sends signals to NotificationService

--- src/main/java/spring/notify/PeriodicSenderServiceImpl.java
+++ src/main/java/spring/notify/PeriodicSenderServiceImpl.java
@@ -1,18 +1,27 @@
 package spring.notify;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.*;
 
 @Component
 public class PeriodicSenderServiceImpl implements PeriodicSenderService {
+    @Value("${spring.notify.periodic.sender.message}")
+    private String message;
+
+    private NotificationService notificationService;
     @Override
     public void send(int count) throws InterruptedException {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext("spring");
+        NotificationService bean = applicationContext.getBean(NotificationService.class);
+
         CountDownLatch lock = new CountDownLatch(count);
 
         ScheduledExecutorService executor = Executors.newScheduledThreadPool(5);
         ScheduledFuture<?> future = executor.scheduleAtFixedRate(() -> {
-            bean.notify( , Importance.LOW);
+            bean.notify(message, Importance.LOW);
             lock.countDown();
         }, 500, 100, TimeUnit.MILLISECONDS);
 
